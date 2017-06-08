bef60d13dcd2d470d15c1e629437be61bbe6babd
Tudor497 <barcan.tudor@gmail.com>
2017-06-08 17:46:29 +0300

    Add files via upload

---
+++ Scripts/PixelPerfectCamera.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PixelPerfectCamera : MonoBehaviour {
+
+	public static float pixelsToUnits = 1f;
+	public static float scale=1f;
+	public static float objRes = 1f;
+    public static Vector3 pos;
+	public Vector2 NativeResolution = new Vector2(240,160);
+	// Use this for initialization
+	void Awake (){
+		var camera = GetComponent<Camera>();
+        pos = camera.gameObject.transform.position;
+        pos.z = 1;
+            if (camera.orthographic) {
+			scale = Screen.width / NativeResolution.y;
+			pixelsToUnits *= scale;
+			camera.orthographicSize = (Screen.height / 2.0f) / pixelsToUnits;
+			objRes = camera.orthographicSize;
+
+		}
+	}
+	
+
+}
---
+++ Scripts/asd.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class asd : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		SceneManager.LoadScene ("Menu");
+		Debug.Log ("HERE");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
