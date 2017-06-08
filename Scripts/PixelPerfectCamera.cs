7e769fa728b01e590ab996cc1e93c5ebf4609fe2
Tudor497 <barcan.tudor@gmail.com>
2017-06-08 17:50:43 +0300

    Add files via upload

--- Scripts/PixelPerfectCamera.cs
+++ Scripts/PixelPerfectCamera.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 
 public class PixelPerfectCamera : MonoBehaviour {
 
-	public static float pixelsToUnits = 1f;
-	public static float scale=1f;
-	public static float objRes = 1f;
+	public static float pixelsToUnits;
+	public static float scale;
+	public static float objRes;
     public static Vector3 pos;
 	public Vector2 NativeResolution = new Vector2(240,160);
 	// Use this for initialization
 	void Awake (){
-		var camera = GetComponent<Camera>();
+      pixelsToUnits = 1f;
+     scale = 1f;
+     objRes = 1f;
+    var camera = GetComponent<Camera>();
         pos = camera.gameObject.transform.position;
         pos.z = 1;
             if (camera.orthographic) {
