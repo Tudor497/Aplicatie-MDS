6062da69bd3391feaac7f8af2b2ebfed5057a246
Tudor497 <barcan.tudor@gmail.com>
2017-06-08 17:55:21 +0300

    Add files via upload

---
+++ Scripts/CameraFollow.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+    [SerializeField]
+    Transform player;
+
+    Vector3 poz;
+    float coord_y;
+
+    void Awake()
+    {
+        poz = new Vector3(transform.position.y, 1, transform.position.z);
+    }
+
+	// Use this for initialization
+	void Start () {
+        coord_y = player.position.y;
+        transform.position = poz + (Vector3.up * coord_y);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        coord_y = Mathf.Max(coord_y, player.position.y);
+
+        transform.position = poz + (Vector3.up * coord_y);
+		
+	}
+}
