3ffbfb7131680058b6028b8308cecc5224335154
Tudor497 <barcan.tudor@gmail.com>
2017-06-08 18:19:27 +0300

    Add files via upload

---
+++ Scripts/Tilling.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tilling : MonoBehaviour {
+
+	// Use this for initialization
+	public Renderer rend;
+	void Start() {
+		rend = GetComponent<Renderer>();
+	}
+	void Update() {
+		float scaleX =  1;
+		float scaleY =  1;
+		rend.material.mainTextureScale = new Vector2(scaleX, scaleY);
+	}
+}
