38fb53a37d56a53b4b80e2c610bb8bebb28e7998
danutscirlat <danut.scirlat@gmail.com>
2017-06-08 17:39:29 +0300

    Add files via upload

---
+++ GetName.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GetName : MonoBehaviour {
+	[SerializeField]
+	Text textField;
+	public static string USERNAME="Guest";
+
+	// Use this for initialization
+	void Start () {
+		
+		textField.text = USERNAME;
+	
+	}
+	
+
+}
